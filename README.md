c130ba8cd73fef6e263893b5880c5b39980f3a5e
Stéphane Gagnon <stephane-gagnon@hotmail.com>
2021-11-17 16:18:57 -0500

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # flaskdings
 A Flask API for MIDIDINGS
+# Goal
+* Provide a REST API layer and the abstraction of the OSC layer
+* Provide a HTML5 frontend as an alternative to the livedings frontend
+# Mididings ressources
+* http://dsacre.github.io/mididings/doc/
+* https://groups.google.com/g/mididings
+
--- src/service.py
+++ src/service.py
@@ -3,10 +3,8 @@
 
 import os
 import liblo
-from flaskdings import FlaskDings
-from time import sleep
-
-from flask import Flask, redirect, url_for, render_template, make_response, request, jsonify, flash
+from models.flaskdings import FlaskDings
+from flask import Flask, redirect, url_for, render_template
 from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
@@ -18,47 +16,48 @@ filename = os.path.join(app.static_folder, 'config.json')
 with open(filename) as f:
     configuration = json.load(f)
 
-flask_dings = None
+flaskdings = None
 if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-    flask_dings = FlaskDings(configuration["server"])
+    flaskdings = FlaskDings(configuration["server"])
+
 
 @app.route("/")
 def index():
-    return render_template('index.html', config=flask_dings)
+    return render_template('index.html', config=flaskdings)
 
 @app.route("/mididings/next_scene")
 def next_scene():
-    flask_dings.osc.next_scene()
+    flaskdings.osc.next_scene()
     return redirect(url_for('index'))
 
 @app.route("/mididings/prev_scene")
 def prev_scene():
-    flask_dings.osc.prev_scene()
+    flaskdings.osc.prev_scene()
     return redirect(url_for('index'))
 
 @app.route("/mididings/next_subscene")
 def next_subscene():
-    flask_dings.osc.next_subscene()
+    flaskdings.osc.next_subscene()
     return redirect(url_for('index'))
 
 @app.route("/mididings/prev_subscene")
 def prev_subscene():
-    flask_dings.osc.prev_subscene()
+    flaskdings.osc.prev_subscene()
     return redirect(url_for('index'))
 
 @app.route("/mididings/panic")
 def panic():
-    flask_dings.osc.panic()
+    flaskdings.osc.panic()
     return ('', 204)
 
 @app.route("/mididings/scenes/<int:value>")
 def switch_scene(value):
-    flask_dings.osc.switch_scene(value)
+    flaskdings.osc.switch_scene(value)
     return redirect(url_for('index'))
 
 @app.route("/mididings/subscenes/<int:value>")
 def switch_subscene(value):
-    flask_dings.osc.switch_subscene(value)
+    flaskdings.osc.switch_subscene(value)
     return redirect(url_for('index'))
 
 # Errors
