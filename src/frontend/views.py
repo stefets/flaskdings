90ff0ac5814f686515a19fdcfc163371556c48e7
Stéphane Gagnon <stephane-gagnon@hotmail.com>
2021-11-23 09:16:34 -0500

    Add the /restart endpoint

--- src/frontend/views.py
+++ src/frontend/views.py
@@ -8,9 +8,14 @@ ui_blueprint = Blueprint('ui_blueprint', __name__,
 
 @ui_blueprint.route("/")
 def index():
-    if current_app.config['livedings'].scenes:
-        # We are here after an OSC process, wait a little before rendering
-        sleep(0.0625)
+    timeout = 0
+    dings_context = current_app.config['livedings']
+    while not dings_context.ready:
+        sleep(0.0625) # We are here after an OSC process, wait a little before rendering
+        timeout += 1
+        if timeout % 32 == 0:
+            break
+    if dings_context.ready and dings_context.scenes:
         return render_template('ui.html', context=current_app.config['livedings'], parameter="?view=")
     else:
         return render_template('nodings.html')
