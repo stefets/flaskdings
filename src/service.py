643ef9c4b0159a207e7393734fdde0b57b2cc201
Stéphane Gagnon <stephane-gagnon@hotmail.com>
2021-11-25 15:38:41 -0500

    Websockets implementation (#8)
    * Basic navigation and header works
    
    * Subscene nav,  input type button
    
    * Render scenes and subscenes buttons from JS
    
    * Revamp layout
    
    * Contextualize button style
    
    * Console WIP
    
    * Better layout
    
    * Send a signal to socket.io on osc scene change notification
    
    * Requirements document
    
    * Update visual document

--- src/service.py
+++ src/service.py
@@ -1,148 +1,132 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from flask_socketio import SocketIO
+from frontend.views import ui_blueprint
+from osc.server import MididingsContext
 import os
 import json
-import jinja2
 
-from osc.server import MididingsContext
-from frontend.views import ui_blueprint
-
-from flask import Flask, redirect, url_for, render_template, jsonify, request, make_response
+''' Flask '''
+from flask import Flask, render_template, jsonify
+from flask.signals import Namespace
 from werkzeug.exceptions import HTTPException
 from werkzeug.utils import import_string
+
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-# Configuration
+''' Signal '''
+namespace = Namespace()
+osc_message = namespace.signal('osc_message')
+
+
+''' Websockets '''
+socketio = SocketIO(app)
+
+
+"""  Configuration """
 filename = os.path.join(app.static_folder, 'config.json')
 with open(filename) as FILE:
     configuration = json.load(FILE)
 
+"""  OSC Service """
 livedings = None
 if not app.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-    livedings = MididingsContext(configuration["osc_server"])
+    livedings = MididingsContext(configuration["osc_server"], osc_message)
 
 
-# BLUEPRINT
+"""  Bluebrint(s) """
 app.config['livedings'] = livedings
 app.register_blueprint(ui_blueprint)
-#
+
+
+'''
+    Api routes
+'''
 
 
 @app.route("/")
 @app.route("/api")
+@app.route("/home")
+@app.route("/index")
 def index():
-    return render_template('api.html')
+    return render_template('index.html')
 
 
-@app.route("/api/mididings/next_scene")
+'''
+    Websockets routes
+'''
+
+
+@osc_message.connect
+def emit_mididings_context(sender=None):
+    socketio.emit('mididings_context_update', {
+                  'current_scene': livedings.current_scene,
+                  'current_subscene': livedings.current_subscene,
+                  'scenes': livedings.scenes})
+
+
+@socketio.event
+def get_mididings_context():
+    emit_mididings_context()
+
+
+@socketio.event
+def switch_scene(data):
+    livedings.switch_scene(int(data['id']))
+
+
+@socketio.event
+def switch_subscene(data):
+    livedings.switch_subscene(int(data['id']))
+
+
+@socketio.event
 def next_scene():
-    return on_api_request(livedings.next_scene)
+    livedings.next_scene()
 
 
-@app.route("/api/mididings/prev_scene")
+@socketio.event
 def prev_scene():
-    return on_api_request(livedings.prev_scene)
+    livedings.prev_scene()
 
 
-@app.route("/api/mididings/next_subscene")
-def next_subscene():
-    return on_api_request(livedings.next_subscene)
-
-
-@app.route("/api/mididings/prev_subscene")
+@socketio.event
 def prev_subscene():
-    return on_api_request(livedings.prev_subscene)
+    livedings.prev_subscene()
 
 
-@app.route("/api/mididings/panic")
-def panic():
-    return on_api_request(livedings.panic)
+@socketio.event
+def next_subscene():
+    livedings.next_subscene()
 
 
-@app.route("/api/mididings/quit")
-def quit():
-    return on_api_request(livedings.quit)
-
-
-@app.route("/api/mididings/restart")
+@socketio.event
 def restart():
-    return on_api_request(livedings.restart)
+    livedings.restart()
 
 
-@app.route("/api/mididings/scenes/<int:value>")
-def switch_scene(value):
-    return on_api_request(livedings.switch_scene, value)
+@socketio.event
+def panic():
+    livedings.panic()
 
 
-@app.route("/api/mididings/subscenes/<int:value>")
-def switch_subscene(value):
-    return on_api_request(livedings.switch_subscene, value)
-
-
-'''
-    Execute the request in a hybrid way - API only or FRONTEND
-    Default is API mode and 204 is returned 
-    If the request.args.view parameters is defined, we redirect to this view
-'''
-
-
-def on_api_request(livedings_action, action_value=None):
-    livedings() # Set the ready flag to False
-    livedings_action(action_value) if action_value else livedings_action()
-    view = request.args.get('view')
-    return make_response('', 204) if not view else redirect(url_for(view))
-
-
-@app.route("/api/help")
-def help():
-    """
-    API Home
-    """
-    routes = []
-    for rule in app.url_map.iter_rules():
-        try:
-            if rule.endpoint != 'static':
-                if hasattr(app.view_functions[rule.endpoint], 'import_name'):
-                    import_name = app.view_functions[rule.endpoint].import_name
-                    obj = import_string(import_name)
-                    routes.append({rule.rule: "%s\n%s" %
-                                  (",".join(list(rule.methods)), obj.__doc__)})
-                else:
-                    routes.append(
-                        {rule.rule: app.view_functions[rule.endpoint].__doc__})
-        except Exception as exc:
-            routes.append({rule.rule:
-                           "(%s) INVALID ROUTE DEFINITION!!!" % rule.endpoint})
-            route_info = "%s => %s" % (rule.rule, rule.endpoint)
-            app.logger.error("Invalid route: %s" % route_info, exc_info=True)
-            # func_list[rule.rule] = obj.__doc__
-
-    return jsonify(code=200, data=routes)
-
-
-@app.route("/api/ping")
-def ping():
-    return '', 204
+@socketio.event
+def quit():
+    livedings.quit()
+    socketio.emit("application_shutdown")
 
 
 '''
-Jinja2 Filters
-This filter serve the API to determine if we render a view
+Error Handling
 '''
 
 
-def inject_endpoint(param):
-    return param + request.endpoint
-
-
-jinja2.filters.FILTERS['inject_endpoint'] = inject_endpoint
-
-'''
-Errors
-'''
+@socketio.on_error()
+def error_handler(e):
+    print("socketio.on_error")
 
 
 @app.errorhandler(HTTPException)
@@ -161,4 +145,4 @@ def handle_exception(e):
 
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
